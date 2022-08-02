7316d03918b59a82bbad0c20782e4b7e7f2eafa1
unconscious123 <3329058005@qq.com>
2022-08-02 09:41:37 +0800

    版本2

--- import matplotlib.py
+++ import matplotlib.py
@@ -3,12 +3,12 @@ from matplotlib import pyplot as plt
 import cv2 as cv
 import numpy as np
 
-def bgr2rgb(img):
-    return img[:,:,::-1]
-
-img=cv.imread("img1.jpg",-1)
-newImage=np.concatenate((img,bgr2rgb(img)),2)
-plt.imshow(newImage)
-plt.show()
-cv.waitKey(0)
-cv.destroyAllWindows()
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    ret,frame = cap.read()
+    if not ret:
+        print("Cannot receive frame")
+        break
